1e852ea772e296f94f6efb433036d6b5bc9610c0
sharansimikore <ssimikore@gmail.com>
2026-01-29 00:13:44 +0530

    merge_test

--- base/base_driver.py
+++ base/base_driver.py
@@ -18,3 +18,9 @@ class BaseDriver:
                 break
             last_height = new_height
 
+    def test_conflict(self):
+        print("conflict testing")
+
+    def test_merge(self):
+        print("merge testing")
+
